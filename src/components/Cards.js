5facbfbc9d2cf9def7971d7b612b7e24e7eab235
durgeshkasle <kasledurgesh@gmail.com>
2023-07-10 20:13:45 +0530

    netflix-app

---
+++ src/components/Cards.js
@@ -0,0 +1,26 @@
+function Cards(){
+
+    return <>
+
+
+
+        <div>
+
+        <img src="https://m.media-amazon.com/images/M/MV5BMDZkYmVhNjMtNWU4MC00MDQxLWE3MjYtZGMzZWI1ZjhlOWJmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"
+        style={{height:250,padding:20,}}
+        alt="card-image"
+        />
+
+        </div>
+    
+        <h3> The Stranger Things </h3>
+
+        </>
+
+}
+
+    
+
+
+
+export default Cards
---
+++ src/components/Logo.js
@@ -0,0 +1,17 @@
+function Logo() {
+
+return(
+    <>
+
+    <img src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1000&h=563&crop=1" alt="logo-image" style={{height:50,padding:20}}/>
+
+
+    
+
+    </>
+)
+
+
+}
+
+export default Logo;
